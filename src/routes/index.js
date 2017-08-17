087690c8795c99342bc3a1a474b06241c2cc6d62
mizutake <takeshi_17n1100069@.nnn.ed.jp>
2017-08-17 16:21:16 +0900

    2017-08-17(3)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,30 @@
+const Toot = require('../models/toot');
+const User = require('../models/user');
+const UserSession = require('../models/user_session');
+
+
+
 module.exports = function(app) {
+
   app.get("/", function(req, res) {
-    res.send("Initialized:" + req.signedCookies.session_id);
+    if(!req.signedCookies.session_id) {
+      rea.redirect('./login');
+      return;
+    }
+    res.render('timeline')
+  });
+
+  app.post('/new_toot', function(req, res) {
+    UserSession.find(req.signedCookies.session_id).then((session) => {
+      return User.find(session.data.user_id);
+    }).then((user) => {
+      return Toot.create(user, req.body.toot);
+    }).then(() => {
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err);
+      res.redirect('/');
+    });
   });
 
   require('./users')(app);
