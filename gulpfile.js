3710150404549049463e8db9990c73c5b47b1209
mizutake <takeshi_17n1100069@.nnn.ed.jp>
2017-08-21 15:24:36 +0900

    2017-08-21(2time)

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const express = require('gulp-express');
+const webpack = require('webpack-stream');
+
+gulp.task('webpack', function(){
+  return gulp.src('frontend/index.js')
+  .pipe(webpack(require('./webpack.config.js')))
+  .pipe(gulp.dest('public/'))
+});
+
+gulp.task('sever', function() {
+  express.run(['src/index.js']);
+
+  gulp.watch(['frontend/**/*.js'], ['webpack']);
+  gulp.watch(['src/**/*.js'], [express.run]);
+})
